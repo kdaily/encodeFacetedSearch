dfb02ce2a6616b2e9521cca56f3c9882bb4e3df3
kdaily <kdaily@users.noreply.github.com>
2016-01-14 16:03:04 -0800

    split making table and rendering UI

--- server.R
+++ server.R
@@ -5,59 +5,59 @@
 # http://shiny.rstudio.com
 #
 
+mktbl <- function(x) {
+  DT::renderDataTable(x,
+                      options = list(lengthChange = FALSE, dom='t',
+                                     ordering=FALSE,
+                                     autoWidth = TRUE,
+                                     nowrap=FALSE,
+                                     columnDefs = list(list(width='200px',
+                                                            targets=0),
+                                                       list(width='100px',
+                                                            targets=1))),
+                      extensions = 'Responsive',
+                      style = 'default',
+                      class='compact',
+                      rownames=FALSE,
+                      escape=1)
+}
+
 library(shiny)
 
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
   
   output$Main <- DT::renderDataTable(
     DT::datatable(df)
   )
   
-  # values <- reactiveValues()
-  # 
-  # observe({
-  #   lapply(names(dfs), 
-  #          function(x) {
-  #            tmp <- dfs[[x]]
-  #            colnames(tmp) <- NULL
-  #            output[[sprintf('table_%s', x)]] <- DT::renderDataTable(tmp,
-  #                                                                    options = list(lengthChange = FALSE, dom='t',
-  #                                                                                   ordering=FALSE,
-  #                                                                                   autoWidth = TRUE,
-  #                                                                                   nowrap=FALSE,
-  #                                                                                   columnDefs = list(list(width='200px', 
-  #                                                                                                          targets=0),
-  #                                                                                                     list(width='100px', 
-  #                                                                                                          targets=1))),
-  #                                                                    extensions = 'Responsive',
-  #                                                                    style = 'default',
-  #                                                                    class='compact',
-  #                                                                    rownames=FALSE,
-  #                                                                    escape=1)
-  #            values[[x]] <- tmp
-  #            return(x)})
-  #   })
+  values <- reactiveValues()
+
+  observe({
+    lapply(names(dfs),
+           function(x) {
+             cat(sprintf("Updating values table_%s\n", x), file=stderr())
+             tmp <- dfs[[x]]
+             colnames(tmp) <- NULL
+             values[[x]] <- tmp
+           })  
+  })
+  
+  observe({
+    lapply(names(values),
+           function(x) {
+             cat(sprintf("Updating output table_%s\n", x), file=stderr())
+             output[[sprintf('table_%s', x)]] <- mktbl(values[[x]])
+             return(x)
+             })
+    })
   
   output$Dynamic <- renderUI({
     lapply(names(dfs), 
            function(x) {
-             print(sprintf("table_", x)) 
-             dataTableOutput(sprintf("table_", x))
+             dataTableOutput(sprintf("table_%s", x))
            })
   })
 
   output$Foo <- renderText({"Foo"})  
-  # output$tbl1 <- DT::renderDataTable(df1,
-  #                                   options = list(lengthChange = FALSE, dom='t'),
-  #                                   extensions = 'Responsive',
-  #                                   style = 'bootstrap',
-  #                                   rownames=FALSE,
-  #                                   escape=1)
-  # output$tbl2 <- DT::renderDataTable(df2,
-  #                                   options = list(lengthChange = FALSE, dom='t'),
-  #                                   extensions = 'Responsive',
-  #                                   style = 'bootstrap',
-  #                                   rownames=FALSE,
-  #                                   escape=1)
-  
+
 })
